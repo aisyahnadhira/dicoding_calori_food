e49a608b7543f54a3a1659a12795af5f4b6d79aa
aisyahnadhira <aisyahaisyah725@gmail.com>
2022-04-10 12:45:59 +0700

    fix scroll direction and add drawer

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -4,46 +4,74 @@ import 'package:google_fonts/google_fonts.dart';
 
 class DetailScreen extends StatelessWidget {
   final CaloriData title;
- 
+
   DetailScreen({required this.title});
+  var scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 83, 79, 79),
-      body: SingleChildScrollView(
-        
-        child: Padding(
-          padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 50.0, bottom: 10.0),
+      key: scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+        backgroundColor: Color.fromARGB(255, 83, 79, 79),
+        body: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.only(
+              left: 24.0, right: 24.0, top: 50.0, bottom: 10.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
                 IconButton(
                   icon: Icon(Icons.arrow_back_ios),
                   onPressed: () {
                     Navigator.pop(context);
-                    },
-                    color: Colors.white,
+                  },
+                  color: Colors.white,
                 ),
-                Text(title.title, 
+                Text(
+                  title.title,
                   style: TextStyle(
-                    fontSize: 24, 
-                    fontWeight: FontWeight.bold, 
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                     fontFamily: GoogleFonts.openSans().fontFamily,
-                    color: Colors.white, 
-                    ),
+                    color: Colors.white,
                   ),
-              IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {},
-                color: Colors.white,
+                ),
+                IconButton(
+                  icon: Icon(Icons.menu, color: Colors.white),
+                  onPressed: () {
+                    scaffoldKey.currentState?.openDrawer();
+                  },
+                  ),
+              ]),
+              SizedBox(
+                height: 20,
               ),
-              ]
-            ),
-            SizedBox(height: 20,),
               Container(
                 height: MediaQuery.of(context).size.height * 0.6,
                 width: MediaQuery.of(context).size.width,
@@ -56,57 +84,67 @@ class DetailScreen extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.all(10.0),
                         child: Container(
-                          height: MediaQuery.of(context).size.height * 0.3,
-                          width: MediaQuery.of(context).size.height * 0.4,
-                          child: Image.asset(title.imageList[0])),
+                            height: MediaQuery.of(context).size.height * 0.3,
+                            width: MediaQuery.of(context).size.height * 0.4,
+                            child: Image.asset(title.imageList[0])),
                       ),
-                      Text(title.foodNameList[0], 
-                      style: TextStyle(
-                        fontSize: 24, 
-                        fontWeight: FontWeight.bold, 
-                        fontFamily: GoogleFonts.openSans().fontFamily, 
-                        color: Colors.white),),
-                       SizedBox(width: 30),
-                      Text(title.kaloriList[0], style: TextStyle(
-                        fontSize: 24, 
-                        fontWeight: FontWeight.bold, 
-                        fontFamily: GoogleFonts.openSans().fontFamily, 
-                        color: Colors.white),),
-                        Favorite(),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                height: MediaQuery.of(context).size.height *0.6,
-                width: MediaQuery.of(context).size.width,
-                child: Card(
-                  color: Colors.black38,
-                  elevation: 5,
-                  child: Column(          
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          height: MediaQuery.of(context).size.height * 0.3,
-                          width: MediaQuery.of(context).size.height *0.4,
-                          child: Image.asset(title.imageList[1])),
+                      Text(
+                        title.foodNameList[0],
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.openSans().fontFamily,
+                            color: Colors.white),
                       ),
-                      Text(title.foodNameList[1],
-                      style: TextStyle(
-                        fontSize: 24, 
-                        fontWeight: FontWeight.bold, 
-                        fontFamily: GoogleFonts.openSans().fontFamily, 
-                        color: Colors.white),),
                       SizedBox(width: 30),
-                      Text(title.kaloriList[1], style: TextStyle(
-                        fontSize: 24, 
-                        fontWeight: FontWeight.bold, 
-                        fontFamily: GoogleFonts.openSans().fontFamily, 
-                        color: Colors.white),),
-                        Favorite(),
+                      Text(
+                        title.kaloriList[0],
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.openSans().fontFamily,
+                            color: Colors.white),
+                      ),
+                      Favorite(),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.6,
+                width: MediaQuery.of(context).size.width,
+                child: Card(
+                  color: Colors.black38,
+                  elevation: 5,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                            height: MediaQuery.of(context).size.height * 0.3,
+                            width: MediaQuery.of(context).size.height * 0.4,
+                            child: Image.asset(title.imageList[1])),
+                      ),
+                      Text(
+                        title.foodNameList[1],
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.openSans().fontFamily,
+                            color: Colors.white),
+                      ),
+                      SizedBox(width: 30),
+                      Text(
+                        title.kaloriList[1],
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.openSans().fontFamily,
+                            color: Colors.white),
+                      ),
+                      Favorite(),
                     ],
                   ),
                 ),
@@ -123,22 +161,28 @@ class DetailScreen extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.all(10.0),
                         child: Container(
-                          height: MediaQuery.of(context).size.height * 0.3,
-                          width: MediaQuery.of(context).size.height * 0.4,
-                          child: Image.asset(title.imageList[2])),
+                            height: MediaQuery.of(context).size.height * 0.3,
+                            width: MediaQuery.of(context).size.height * 0.4,
+                            child: Image.asset(title.imageList[2])),
                       ),
-                      Text(title.foodNameList[2], style: TextStyle(
-                        fontSize: 24, 
-                        fontWeight: FontWeight.bold, 
-                        fontFamily: GoogleFonts.openSans().fontFamily, 
-                        color: Colors.white),),
-                       SizedBox(width: 30),
-                      Text(title.kaloriList[2], style: TextStyle(
-                        fontSize: 24, 
-                        fontWeight: FontWeight.bold, 
-                        fontFamily: GoogleFonts.openSans().fontFamily, 
-                        color: Colors.white),),
-                        Favorite(),
+                      Text(
+                        title.foodNameList[2],
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.openSans().fontFamily,
+                            color: Colors.white),
+                      ),
+                      SizedBox(width: 30),
+                      Text(
+                        title.kaloriList[2],
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.openSans().fontFamily,
+                            color: Colors.white),
+                      ),
+                      Favorite(),
                     ],
                   ),
                 ),
@@ -155,22 +199,28 @@ class DetailScreen extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.all(10.0),
                         child: Container(
-                          height: MediaQuery.of(context).size.height * 0.3,
-                          width: MediaQuery.of(context).size.height * 0.4,
-                          child: Image.asset(title.imageList[3])),
+                            height: MediaQuery.of(context).size.height * 0.3,
+                            width: MediaQuery.of(context).size.height * 0.4,
+                            child: Image.asset(title.imageList[3])),
                       ),
-                      Text(title.foodNameList[3], style: TextStyle(
-                        fontSize: 24, 
-                        fontWeight: FontWeight.bold, 
-                        fontFamily: GoogleFonts.openSans().fontFamily, 
-                        color: Colors.white),),
-                       SizedBox(width: 30),
-                      Text(title.kaloriList[3], style: TextStyle(
-                        fontSize: 24, 
-                        fontWeight: FontWeight.bold, 
-                        fontFamily: GoogleFonts.openSans().fontFamily, 
-                        color: Colors.white),),
-                        Favorite(),
+                      Text(
+                        title.foodNameList[3],
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.openSans().fontFamily,
+                            color: Colors.white),
+                      ),
+                      SizedBox(width: 30),
+                      Text(
+                        title.kaloriList[3],
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.openSans().fontFamily,
+                            color: Colors.white),
+                      ),
+                      Favorite(),
                     ],
                   ),
                 ),
@@ -179,15 +229,15 @@ class DetailScreen extends StatelessWidget {
             ],
           ),
         )
-      )
-    );
+        ),
+        
+        
+        );
   }
 }
 
 class Favorite extends StatefulWidget {
-  Favorite({ Key? key }) : super(key: key);
-
-  // final data = caloriDataList;
+  Favorite({Key? key}) : super(key: key);
 
   @override
   State<Favorite> createState() => _FavoriteState();
--- lib/pages/main_Screen.dart
+++ lib/pages/main_Screen.dart
@@ -4,49 +4,52 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:calori_food/models/CaloriData.dart';
 
 class MainScreen extends StatelessWidget {
-  MainScreen({ Key? key }) : super(key: key);
+  MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 83, 79, 79),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 50.0, bottom: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back_ios),
-                  onPressed: () {
-                    Navigator.pop(context);
-                    },
-                    color: Colors.white,
-                ),
-                Text('Explore', 
-                  style: TextStyle(
-                    fontSize: 24, 
-                    fontWeight: FontWeight.bold, 
-                    fontFamily: GoogleFonts.openSans().fontFamily,
-                    color: Colors.white, 
+        backgroundColor: Color.fromARGB(255, 83, 79, 79),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 24.0, right: 24.0, top: 50.0, bottom: 10.0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      color: Colors.white,
                     ),
-                  ),
-              IconButton(
-                icon: Icon(Icons.list),
-                onPressed: () {},
-                color: Colors.white,
-              ),
-              ]
+                    Text(
+                      'Explore',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: GoogleFonts.openSans().fontFamily,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Icon(
+                      Icons.restaurant_menu,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ]),
             ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(24.0),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
               child: TextField(
                 keyboardType: TextInputType.name,
                 decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search, color: Colors.black,),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
                   fillColor: Color.fromARGB(255, 238, 237, 240),
                   filled: true,
                   hintText: "Search Food",
@@ -59,72 +62,79 @@ class MainScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(13.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                height: 42,
-                width: 100,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color.fromARGB(255, 253, 93, 1),
-                      Color.fromARGB(255, 236, 146, 42),
-                    ],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                  ),
-                  borderRadius: BorderRadius.circular(24.0),
-                ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                      child: Text(
-                        'All',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        fontFamily: GoogleFonts.montserrat().fontFamily,
-                          color: Colors.white,
+            ),
+            
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height * 0.6,
+            ),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              child: ListView.builder(        
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.only(left: 12, bottom: 16),
+                itemCount: caloriDataList.length,
+                itemBuilder: (context, index) {
+                  final CaloriData food = caloriDataList[index];
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return DetailScreen(title: food);
+                      }));
+                    },
+                    child: Container(
+                     
+                      child: Stack(children: <Widget>[
+                        Positioned(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 19),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Stack(
+                                children: <Widget>[
+                                  Positioned.fill(
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                        gradient: LinearGradient(
+                                          colors: <Color>[
+                                            Color.fromARGB(255, 253, 93, 1),
+                                            Color.fromARGB(255, 236, 146, 42),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                                      child: TextButton(
+                                        onPressed: () {},
+                                        child: Text(food.title,
+                                                style: TextStyle(
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.w600,
+                                                  fontFamily: 'Montserrat',
+                                                  color: Colors.white,
+                                                ),),
+                                      ),
+                                    ),
+                                  ),
+                                ]
+                              )
+                            )
+                          
+                          ),
                         ),
-                      ),
+                        
+                      ]),
                     ),
-                  ),
-                
-                ),
-                Text("Bread", 
-                  style: TextStyle(
-                    fontSize: 20, 
-                    fontWeight: FontWeight.w400, 
-                    fontFamily: GoogleFonts.montserrat().fontFamily,
-                    color: Colors.white, 
-                    ),
-                  ),
-                  Text("Drink", 
-                style: TextStyle(
-                  fontSize: 20, 
-                  fontWeight: FontWeight.w400, 
-                  fontFamily: GoogleFonts.montserrat().fontFamily,
-                  color: Colors.white, 
-                  ),
-                ),
-                Text("Fruit", 
-                style: TextStyle(
-                  fontSize: 20, 
-                  fontWeight: FontWeight.w400, 
-                  fontFamily: GoogleFonts.montserrat().fontFamily,
-                  color: Colors.white, 
-                  ),
-                ),
-              ],
+                  );
+                },
+              ),
             ),
           ),
-
           Expanded(
             child: Container(
               child: ListView.builder(
@@ -181,22 +191,20 @@ class MainScreen extends StatelessWidget {
                                               fontWeight: FontWeight.w700,
                                             fontFamily: GoogleFonts.montserrat().fontFamily,
                                               color: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
+                                    ),
                                   ),
                                 ),
-                              ]
+                              ),
                             ),
-                    ),
-                  );
-                },
+                          ),
+                        ]),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        ],
-      )
-    );
+          ],
+        ));
   }
 }
